a44dc83fe8b1eb935f45e955f51f75bb75cb4f6d
Bruno Ferreira <bruno.ferreira@ifmg.edu.br>
2021-11-08 10:39:22 -0300

    first commit for vercel

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './navbar.css';
+
+export function NavBar({dados}){
+
+    
+    return(
+        <>               
+        <nav className="navbar navbar-default navbar-fixed-top">
+            <div className="container">
+                <div className="navbar-header">
+                    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#menu-navegacao" >
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>  
+                                
+                    <Link to={window.location.pathname === "/" ? "#" : "/"} onClick={()=>{
+                        window.scrollTo(0,0)
+                    }} className="navbar-brand">{dados.data.nomedaempresa[0].text}</Link> 
+                </div>  
+                
+                <div className="collapse navbar-collapse menu-navegacao" id="menu-navegacao">
+                    <ul className="nav navbar-nav navbar-right">      
+                        {window.location.pathname === "/" ? 
+                        <>                                         
+                        <li>
+                            <a className="" href="#campanha">Campanhas</a>
+                        </li>
+                        <li>
+                            <a className="" href="#servicos">Serviços</a>
+                        </li>
+                        <li>
+                            <a className="" href="#quemsomos">Quem Somos</a>
+                        </li>
+                        <li>
+                            <a className="" href="#localizacao">Localização</a>
+                        </li>
+                        <li>
+                            <a className="" href="#contato">Contato</a>
+                        </li>
+                        </>
+                        :
+                        <>
+                       {/* <li>
+                            <a className="" href="#campanha">Campanha</a>
+                        </li>
+                        <li>
+                            <a className="" href="#galeria">Galeria</a>
+                        </li>
+                        <li>
+                            <a className="" href="#quemsomos">Quem Somos</a>
+                        </li>
+                        <li>
+                            <a className="" href="#doacao">Doar</a>
+                       </li>*/}
+                        <li>
+                            <Link to="/#">Voltar</Link>                            
+                        </li>
+                        </>
+                        }
+                    </ul>
+                </div>
+                
+            </div>
+        </nav>
+        </>
+    );
+}
